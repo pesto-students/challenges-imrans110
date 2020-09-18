994c5df4b70c0b1acf78a79f7748345ff6dc3b52
Syed Imran Abidi <imrans110@hotmail.com>
2020-09-18 21:26:57 +0530

    Add Test Cases for flipArgs

--- js-exercises/flip-args/flipArgs.test.js
+++ js-exercises/flip-args/flipArgs.test.js
@@ -1,1 +1,12 @@
-// write your own test cases
+import { flipArgs } from "./flipArgs";
+
+describe("flipArgs", () => {
+  it("should return a function", () => {
+    expect(typeof flipArgs()).toBe("function");
+  });
+  it("Should Call the function passed with reversed arguments", () => {
+    const foo = (x, y) => x - y;
+    const flippedFoo = flipArgs(foo);
+    expect(flippedFoo(5, 10)).toBe(5);
+  });
+});
